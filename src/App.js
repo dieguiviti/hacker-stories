02ce28b58194beff80128425ef7ea9bd2af594b2
Diego Gallovich <dieguiviti@gmail.com>
2020-12-05 14:40:54 +0100

    reformatting from function declarations to arrow functions declarations

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const list = [
   },
 ]
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>Hacker Stories</h1>
@@ -36,23 +36,21 @@ function App() {
   )
 }
 
-function List() {
+const List = () => {
   return (
     <div>
-      {list.map(function (item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>
-                {item.title}
-              </a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        )
-      })}
+      {list.map((item) => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>
+              {item.title}
+            </a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      ))}
     </div>
   )
 }
