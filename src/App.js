5884e09301614abe8e965a575ad60ceea50c0b45
Diego Gallovich <dieguiviti@gmail.com>
2020-12-05 14:53:43 +0100

    Putting stories variable inside the scope of the App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import './App.css'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
-
 const App = () => {
+  // Stories
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
+
   // Change Handler
   const handleChange = (e) => {
     console.log(e.target.value)
@@ -40,15 +41,15 @@ const App = () => {
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   )
 }
 
-const List = () => {
+const List = (props) => {
   return (
     <div>
-      {list.map((item) => (
+      {props.list.map((item) => (
         <div key={item.objectID}>
           <span>
             <a href={item.url}>
